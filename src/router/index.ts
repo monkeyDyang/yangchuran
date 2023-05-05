1eda33b247cb8b8150b41386801cb69ff613c240
YangYang <1059376437@qq.com>
2023-05-05 22:30:09 +0800

    增加时间轴界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TimeLine from "../views/TimeLine.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
@@ -10,7 +12,12 @@ const routes = [
   {
     path: "/login",
     name: "login",
-    component: () => import("../views/LoginView.vue"),
+    component: LoginView,
+  },
+  {
+    path: "/timeline",
+    name: "timeline",
+    component: TimeLine,
   },
 ];
 
---
+++ src/views/TimeLine.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="timeline-container">
+    <!-- TODO 滑动时间轴时，将当前中间的时间轴节点对应的图片设置为背景图片-->
+    <a-timeline mode="alternate">
+      <a-timeline-item>
+        <img class="timeline-image" src="../assets/image/bg.jpg" alt="background image" />
+        2022年11月15日
+      </a-timeline-item>
+      <a-timeline-item>
+        <img class="timeline-image" src="../assets/image/bg.jpg" alt="background image" />
+        2022年12月15日
+      </a-timeline-item>
+      <a-timeline-item>
+        <img class="timeline-image" src="../assets/image/bg.jpg" alt="background image" />
+        2023年1月15日
+      </a-timeline-item>
+      <a-timeline-item>
+        <img class="timeline-image" src="../assets/image/bg.jpg" alt="background image" />
+        2023年2月15日
+      </a-timeline-item>
+      <a-timeline-item>
+        <img class="timeline-image" src="../assets/image/bg.jpg" alt="background image" />
+        2023年3月15日
+      </a-timeline-item>
+    </a-timeline>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+.timeline-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5em 15em;
+}
+
+.timeline-image {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
