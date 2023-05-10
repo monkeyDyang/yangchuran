f5875bba1cd4482267df7d86aceaba7d2d516ef0
YangYang <1059376437@qq.com>
2023-05-10 22:22:24 +0800

    引入 tailwindcss，引入daisyui

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "./assets/css/app.css";
 import "./assets/css/main.css";
 import "./assets/css/base.css";
 
