eac1da6fad2df38728f8b3f8a620b14f43334b81
YangYang <1059376437@qq.com>
2023-05-10 23:12:54 +0800

    删除不需要的文件

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/css/app.css";
-import "./assets/css/main.css";
-import "./assets/css/base.css";
 
 const app = createApp(App);
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import TimeLine from "../views/TimeLine.vue";
-import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginView,
   },
   {
     path: "/timeline",
