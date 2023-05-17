34117719ad555d8791c12b4da204eddb33f81fdf
YangYang <1059376437@qq.com>
2023-05-18 00:06:01 +0800

    🔧 精简vite配置文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
 
-import "./assets/css/app.css";
+import "@/assets/css/app.css";
 
 const app = createApp(App);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
-import { URL, fileURLToPath } from "node:url";
-
-import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -9,7 +6,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": "/src",
     },
   },
   server: {
@@ -18,30 +15,6 @@ export default defineConfig({
         target: "http://192.168.0.104:13191", // 实际请求地址
         changeOrigin: true,
         rewrite: path => path.replace(/^\/userBusiness/, ""),
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          "primary-color": "#1890ff", // 全局主色
-          "link-color": "#1890ff", // 链接色
-          "success-color": "#52c41a", // 成功色
-          "warning-color": "#faad14", // 警告色
-          "error-color": "#f5222d", // 错误色
-          "font-size-base": "14px", // 主字号
-          "heading-color": "rgba(0, 0, 0, 0.85)", // 标题色
-          "text-color": "rgba(0, 0, 0, 0.65)", // 主文本色
-          "text-color-secondary": "rgba(0, 0, 0, 0.45)", // 次文本色
-          "disabled-color": "rgba(0, 0, 0, 0.25)", // 失效色
-          "border-radius-base": "4px", // 组件/浮层圆角
-          "border-color-base": "#d9d9d9", // 边框色
-          "box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.15)", // 浮层阴影
-        },
-        javascriptEnabled: true,
-        // 引入less全局变量
-        additionalData: `@import "${path.resolve(__dirname, "src/assets/less/global.less")}";`,
       },
     },
   },
