7be06207728f254974d8d159753ca078a665e761
YangYang <1059376437@qq.com>
2023-05-21 23:40:57 +0800

    💄 修改footer样式

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="footer footer-center items-center p-4 bg-neutral text-neutral-content">
+  <div class="footer footer-center items-center p-4 bg-base-100 text-base-100-content">
     <a target="_blank" rel="noopener" href="http://www.beian.gov.cn">湘ICP备2023008600号-1</a>
   </div>
 </template>
