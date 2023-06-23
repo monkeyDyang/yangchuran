afef0f83956608cfca165461d5d2fc77946abd95
YangYang <1059376437@qq.com>
2023-06-23 22:49:08 +0800

    🎨 格式化 实现响应式滚动时间轴，线性的展示图片 Fixes #55

--- src/pages/timeline.vue
+++ src/pages/timeline.vue
@@ -12,9 +12,9 @@ export default {
         {
           id: 1,
           icon: "streamline-emojis:baby-bottle",
-          image:{
-            name:"0001.jpg",
-            url:"../src/assets/image/0001.jpg",
+          image: {
+            name: "0001.jpg",
+            url: "../src/assets/image/0001.jpg",
           },
           date: "2022年11月15日",
           description: "出生第 1 天",
@@ -22,9 +22,9 @@ export default {
         {
           id: 2,
           icon: "streamline-emojis:baby-1",
-          image:{
-            name:"0002.jpg",
-            url:"../src/assets/image/0002.jpg",
+          image: {
+            name: "0002.jpg",
+            url: "../src/assets/image/0002.jpg",
           },
           date: "2022年12月15日",
           description: "出生第 30 天",
@@ -32,16 +32,16 @@ export default {
         {
           id: 3,
           icon: "streamline-emojis:crown",
-          image:{
-            name:"bg.jpg",
-            url:"../src/assets/image/bg.jpg",
+          image: {
+            name: "bg.jpg",
+            url: "../src/assets/image/bg.jpg",
           },
           date: "2023年1月15日",
           description: "出生第 60 天",
-        }
-      ]
-    }
-  }
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -50,14 +50,14 @@ export default {
     <div class="relative overflow-hidden py-10 h-full">
       <div v-for="item in timelineData" :key="item.id">
         <!-- Timeline -->
-        <div class="relative pb-8 flex items-center w-full timeline"
-          :class="item.id % 2 === 0 ? 'flex-row-reverse' : ''">
+        <div class="relative pb-8 flex items-center w-full timeline" :class="item.id % 2 === 0 ? 'flex-row-reverse' : ''">
           <!-- Center vertical line -->
           <div class="absolute left-1/2 h-full border-l-2 border-right border-primary" />
           <!-- Icon -->
           <div
-            class="absolute w-8 h-8 -ml-4 order-1 z-20 flex items-center shadow-xl justify-center rounded-full left-1/2 bg-primary">
-            <Icon :icon=item.icon />
+            class="absolute w-8 h-8 -ml-4 order-1 z-20 flex items-center shadow-xl justify-center rounded-full left-1/2 bg-primary"
+          >
+            <Icon :icon="item.icon" />
           </div>
           <!-- Horizontal line -->
           <div class="relative w-1/6 h-1 z-0 bg-primary" :class="item.id % 2 === 0 ? 'right-1/2' : 'left-1/2'" />
@@ -65,7 +65,7 @@ export default {
           <div class="relative card w-1/4 shadow-xl bg-primary" :class="item.id % 2 === 0 ? 'right-1/2' : 'left-1/2'">
             <!-- Image -->
             <figure>
-              <img :src=item.image?.url :alt=item.image?.name>
+              <img :src="item.image?.url" :alt="item.image?.name">
             </figure>
             <!-- description -->
             <div class="card-body">
